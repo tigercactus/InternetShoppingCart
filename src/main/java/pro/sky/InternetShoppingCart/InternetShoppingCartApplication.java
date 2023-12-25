486220b233603f25209741e986343b8367be95d0
tigercactus <tigercactus@yandex.ru>
2023-12-25 22:19:30 +0300

    Initial commit

---
+++ src/main/java/pro/sky/InternetShoppingCart/InternetShoppingCartApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.InternetShoppingCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetShoppingCartApplication.class, args);
+	}
+
+}
